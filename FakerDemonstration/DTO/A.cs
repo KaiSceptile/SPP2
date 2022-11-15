10d07ccf7abcba864e080bd57d65770c519fceaa
KaiSceptile <90106146+KaiSceptile@users.noreply.github.com>
2022-11-15 16:08:20 +0300

    adding faker

---
+++ FakerDemonstration/DTO/A.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerDemonstration.DTO
+{
+    public class A
+    {
+        public B B { get; set; }
+    }
+
+    public class B
+    {
+        public C C { get; set; }
+    }
+
+    public class C
+    {
+        public A A { get; set; } // циклическая зависимость, 
+        // может быть на любом уровне вложенности
+    }
+
+}
---
+++ FakerDemonstration/DTO/Boo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FakerDemonstration.DTO
+{
+        public class Boo
+        {
+            // тестовая переменная для доп. задания. Должно быть равно рандомному значения
+            public int CCC;
+
+            public int[] IntArray;
+            public float d { get; set; }
+            public int a { get; set; }
+
+            public char b;
+            public DateTime c { get; set; }
+
+            private int e;
+
+            public Boo(int a, char b, DateTime c)
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+
+            public int GetE()
+            {
+                return e;
+            }
+
+        }
+
+    }
+
